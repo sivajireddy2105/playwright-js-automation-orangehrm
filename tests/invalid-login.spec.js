0529e149632d8bb76a4ed4d8c5ad4818d5c8fbf8
Shivaji <maramsivaji@gmail.com>
2026-09-06 12:36:53 +0530

    assertion issue is being fixed

--- tests/invalid-login.spec.js
+++ tests/invalid-login.spec.js
@@ -1,16 +1,14 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-import {LoginPage} from '../pages/LoginPage'
+import { LoginPage } from '../pages/LoginPage'
 
-test('TC02 - Verify that the application rejects invalid authentication credentials and the user remains unauthenticated.', async({page})=>{
+test('TC02 - Verify that the application rejects invalid authentication credentials and the user remains unauthenticated.', async ({ page }) => {
     const loginPage = new LoginPage(page)
-    
+
     await page.goto('/')
 
     await loginPage.login('Admin', 'Admin1234')
 
-    await loginPage.invalidCredentialsError.waitFor({state: 'visible'})
-    expect(await loginPage.invalidCredentialsError).toBeVisible()
-
-    await page.pause()
+    await loginPage.invalidCredentialsError.waitFor({ state: 'visible' })
+    await expect(loginPage.invalidCredentialsError).toBeVisible()
 })

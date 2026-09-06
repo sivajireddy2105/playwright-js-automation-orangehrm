c446dcb77c54571f3486db988fdebfc5b876526a
Shivaji <maramsivaji@gmail.com>
2026-09-06 11:54:07 +0530

    removed the headless behavior globally

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
     baseURL: 'https://opensource-demo.orangehrmlive.com/',
-    headless: false,
+    // headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
